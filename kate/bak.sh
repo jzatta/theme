2a56fa7a7790e7625bff412724bb5db5458b0888
Juliano Zatta <julianokzatta@gmail.com>
2017-09-01 20:37:55 -0300

    kate files added

---
+++ kate/bak.sh
@@ -0,0 +1,7 @@
+#/bin/sh
+
+cp -p ~/.config/katepartrc .
+cp -p ~/.config/katerc .
+cp -p ~/.config/kateschemarc .
+cp -p ~/.config/katesyntaxhighlightingrc .
+cp -p ~/.config/katevirc .
---
+++ kate/install.sh
@@ -0,0 +1,7 @@
+#/bin/sh
+
+cp -p katepartrc ~/.config/
+cp -p katerc ~/.config/
+cp -p kateschemarc ~/.config/
+cp -p katesyntaxhighlightingrc ~/.config/
+cp -p katevirc ~/.config/
